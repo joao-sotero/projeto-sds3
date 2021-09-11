cc49718234487ccf8464996fc767bd27c7fac4b3
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2021-09-11 11:04:13 -0300

    project created

---
+++ backend/src/main/java/com/sotero/stvendas/StvendasApplication.java
@@ -0,0 +1,13 @@
+package com.sotero.stvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sotero/stvendas/StvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotero.stvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
