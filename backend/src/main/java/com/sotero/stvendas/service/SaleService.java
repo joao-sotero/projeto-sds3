8c380445faf37049d763781af20bed432c8bf568
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2021-09-12 22:33:34 -0300

    pagination

--- backend/src/main/java/com/sotero/stvendas/service/SaleService.java
+++ backend/src/main/java/com/sotero/stvendas/service/SaleService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sotero.stvendas.DTO.SaleDTO;
 import com.sotero.stvendas.entities.Sale;
 import com.sotero.stvendas.repositories.SaleRepository;
+import com.sotero.stvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,7 +17,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	
